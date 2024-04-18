32e4d961e3cdd08118d321feee143226a67791c9
007Miles <milendrafernando@gmail.com>
2024-04-18 22:28:21 +0530

    feat: create product

---
+++ product-service/src/main/java/com/ctse/productservice/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.ctse.productservice.controller;
+
+import com.ctse.productservice.dto.ProductRequest;
+import com.ctse.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+
+}
---
+++ product-service/src/main/java/com/ctse/productservice/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.ctse.productservice.service;
+
+import com.ctse.productservice.dto.ProductRequest;
+import com.ctse.productservice.model.Product;
+import com.ctse.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getName());
+    }
+}
